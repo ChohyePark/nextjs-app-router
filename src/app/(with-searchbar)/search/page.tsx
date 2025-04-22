febb25d80b62b3a0fd09dc3f975d5c906a09b404
park cho hye <chohye0831@gmail.com>
2025-04-22 12:41:42 +0900

    풀라우트 캐시 실습

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -8,7 +8,7 @@ export default async function Page({
 }) {
   const {q} = await searchParams;
   const response = await fetch(`
-    ${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`)
+    ${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, {cache : "force-cache"})
 
   if(!response.ok) {
     return <div>문제가 발생했습니다</div>
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,4 +1,13 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+
+// export const dynamicParams = false; 
+// 위와 같이 설정하면 generateStaticParams로 넘긴 url 외의 경로로 접근하면 404 페이지로 이동함
+
+export function generateStaticParams () {
+  return [{id : "1"}, {id : "2"} , {id : "3"}];
+} // 미리 도서 데이터 정보를 넘겨줌 
 
 export default async function Page({
   params,
@@ -8,7 +17,11 @@ export default async function Page({
   
   const {id: bookId} = await params;
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`)
+
   if (!response.ok) {
+    if(response.status === 404) {
+      notFound();
+    }
     return <div>오류가 발생했습니다.</div>
   }
 

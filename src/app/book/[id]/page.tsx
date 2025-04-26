2ea495a79dc943cdbd15af6dcc1472d4693a20a8
park cho hye <chohye0831@gmail.com>
2025-04-26 18:00:51 +0900

    리뷰 조회 기능 구현

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,19 +1,13 @@
 import { notFound } from 'next/navigation';
 import style from './page.module.css';
+import ReviewEditor from '@/components/review-editor';
+import { ReviewData } from '@/types';
+import ReviewItem from '@/components/review-item';
 
 // export const dynamicParams = false;
 // 위와 같이 설정하면 generateStaticParams로 넘긴 url 외의 경로로 접근하면 404 페이지로 이동함
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }, { id: '3' }];
-} // 미리 도서 데이터 정보를 넘겨줌
-
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  const { id: bookId } = await params;
+async function BookDetail({ bookId }: { bookId: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
   );
@@ -31,7 +25,7 @@ export default async function Page({
     book;
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -44,6 +38,42 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+async function ReviewList({ bookId }: { bookId: string }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
+  );
+
+  // 에러 처리
+  if (!response.ok) {
+    throw new Error(`Review fetch failed : ${response.statusText}`);
+  }
+
+  const reviews: ReviewData[] = await response.json();
+
+  return (
+    <section>
+      {reviews.map((review) => (
+        <ReviewItem key={`review-item-${review.id}`} {...review} />
+      ))}
+    </section>
+  );
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id: bookId } = await params;
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={bookId} />
+      <ReviewEditor bookId={bookId} />
+      <ReviewList bookId={bookId} />
     </div>
   );
 }
